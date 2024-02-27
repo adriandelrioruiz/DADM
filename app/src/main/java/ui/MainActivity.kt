b456fd0f9a8fde9e5d5145a376949be2d7eaecef
adrian <adriandelrioruiz0@gmail.com>
2024-02-27 15:15:43 +0100

    Hasta mitad de ejercicio 07

--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -2,19 +2,50 @@ package ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationBarView
 import dadm.aidelrio.quotationshake.R
 import dadm.aidelrio.quotationshake.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MenuProvider{
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Añade esta instancia como proveedor de menús a la barra de acción
+        addMenuProvider(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_favourites)
         val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.bottomNavigationView as NavigationBarView
         setContentView(binding.root)
-        val navController = binding.navHostFragment.getFragment<NavHostFragment>().navController
+        navController = binding.navHostFragment.getFragment<NavHostFragment>().navController
         binding.bottomNavigationView.setupWithNavController(navController)
+        // establecemos la barra creada como nueva barra de acción
+        setSupportActionBar(binding.materialToolbar)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.favouritesFragment, R.id.aboutFragment, R.id.settingsFragment, R.id.newQuotationFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_about, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        if (menuItem.itemId == R.id.aboutFragment)
+        {
+            navController.navigate(R.id.aboutFragment)
+            return true
+        }
+        return false
     }
 
 }
--- app/src/main/java/ui/about/AboutDialogFragment.kt
+++ app/src/main/java/ui/about/AboutDialogFragment.kt
@@ -1,7 +1,10 @@
 package ui.about
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.DialogFragment
 import dadm.aidelrio.quotationshake.R
+import dadm.aidelrio.quotationshake.databinding.FragmentAboutBinding
 
 class AboutDialogFragment : DialogFragment(R.layout.fragment_about) {
 
